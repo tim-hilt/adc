e050c906aa82638c224661c37ac12f7192558336
Tim Hilt <timhilt@live.de>
2020-10-19 16:38:14 +0200

    Working on basic functions

--- adc/adc.py
+++ adc/adc.py
@@ -1,28 +1,76 @@
 """
-DOCSTRING
+File: adc.py
+Author: Tim Hilt
+Date: 2020-10-19
 """
 
 import numpy as np
+from numpy.linalg import norm
 from sklearn.cluster import KMeans
 
+
 class ADC:
-    def __init__(self):
-        pass
+    def __init__(self, m=16, bs=8):
+        self.kmeans = None
+        self.database = None
+        self.m = m
+        self.bs = bs
+        self.k = 2**bs
 
-    def fit(self):
-        pass
+    def _train(self, X):
+        self.kmeans = []
+        n, d = X.shape
+        for i in range(self.m):
+            self.kmeans.append(KMeans(self.k)
+                               .fit(X[:, i * (d / self.m):(i + 1) * (d / self.m)]))  # Subsequently train Kmeans-models
+        self.database = self.transform(X)
 
-    def predict(self):
-        pass
+    def fit(self, X):
+        """Fit KMeans-models to a database-matrix
 
-    def transform(self):
-        pass
+        Parameters
+        ----------
+        X : array, shape(n, d)
+            Initial non-quantized vectors
 
-    def fit_transform(self):
-        pass
+        Returns
+        -------
+        self : ADC()
+            Returns an instance of itself
+        """
+        self._train(X)
+        return self
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X))
+
+    def predict_proba(self, X):
+        n, d = X.shape
+
+        lut = np.zeros((len(self.database), self.m))
+        # TODO: Could this be vectorized, so that i wouldn't have to loop over both dimensions?
+        for i in range(lut.shape[0]):
+            for j in range(lut.shape[1]):
+                dist = X[j * (d / self.m):(j + 1) * (d / self.m)] - self.kmeans[j].cluster_centers_[self.database[i, j]]
+                lut[i, j] = dist @ dist
+
+        scores = lut.sum(axis=1)
+        return scores
+
+    def transform(self, X):
+        n, d = X.shape
+        tmp = []
+        for i in range(self.m):
+            # uint8 is ok for everything that's smaller than 256 values; maybe there is a better way
+            tmp.append(self.kmeans[i].predict(X[:, i * (d / self.m):(i + 1) * (d / self.m)]).astype(np.uint8))
+        return np.hstack(tmp)
+
+    def fit_transform(self, X):
+        self._train(X)
+        return self.database
 
     def __repr__(self):
-        pass
+        return f"ADC(m={self.m}, bs={self.bs})"
 
     def __str__(self):
-        pass
+        return f"ADC(m={self.m}, bs={self.bs})"
